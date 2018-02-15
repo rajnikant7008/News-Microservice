500fd6329bc4de5904ea6c0e70ea08403182800b
RajniKant Raman <rajnik914@gmail.com>
2018-02-15 17:22:54 +0530

    SpringFox - Swagger2 API documentation  - Added documentations for APIs

--- src/main/java/com/rajnikant/springAPI/model/Employee.java
+++ src/main/java/com/rajnikant/springAPI/model/Employee.java
@@ -1,12 +1,15 @@
 package com.rajnikant.springAPI.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Data
 public class Employee {
 
+    @ApiModelProperty(notes = "The database generated employee ID")
     private int id;
 
+    @ApiModelProperty(notes = "employee name")
     private String name;
 
 }
